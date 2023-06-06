3e28b77fe6ab76a065f0426c7da096e6a1db2754
HectorValadez <125400006+HectorValadez@users.noreply.github.com>
2023-06-06 13:02:05 -0600

    Add files via upload

--- main.js
+++ main.js
@@ -1,3 +1,17 @@
+var modelo =ml5.imageClassifier("https://teachablemachine.withgoogle.com/models/DE6g-mBkS/model.json",Steve);
+function Steve() {
+    console.log("Steve termino su casa");
+}
+function encontrar() {
+    var foto =document.getElementById("foto");
+    modelo.classify(foto,Tony);
+
+}
+function Tony(error,resultado) {
+    if (!error) {
+        console.log(resultado);
+    }
+} 
 Webcam.set({
     width:265,
     height:265,
@@ -10,6 +24,6 @@ Webcam.on('load', function () {
 })
 function capturar() {
     Webcam.snap(function (ip) {
-        document.getElementById("captura").innerHTML='<img src="'+ip+'">'
+        document.getElementById("captura").innerHTML='<img src="'+ip+'" id="foto">'
     })
 }
